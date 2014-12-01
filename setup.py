85f1392263742554acb09f02f7f4c607ef98c41d
Christoph Buelter <c.buelter@arcor.de>
2014-12-01 20:17:34 +0100

    Add readme, requirements and setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+# :coding: utf-8
+
+import setuptools
+
+setuptools.setup(
+    name="slbsl",
+    version="0.1.0",
+    description="Convert slashes in text on commandline or in clipboard.",
+    long_description=open("README.rst").read(),
+    keywords="slash, backslash, linux, unix, windows, conversion",
+    url="https://bitbucket.org/cb109/slbsl.git",
+    author="Christoph Buelter",
+    author_email="c.buelter@arcor.de",
+
+    packages=setuptools.find_packages(),
+    install_requires=[
+        open("requirements.txt").readlines()
+    ],
+
+
+    entry_points={
+        "console_scripts": [
+            "sl=slbsl:sl",
+            "bsl=slbsl:bsl"
+        ],
+    }
+)
+
