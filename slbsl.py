52f8b784c743d22170f1704b32979ff359a98195
Christoph Buelter <c.buelter@arcor.de>
2015-10-07 00:07:40 +0200

    Add docstrings for all functions.

--- slbsl.py
+++ slbsl.py
@@ -28,20 +28,25 @@ closepar = ")"
 
 
 def _getCommandlineInput():
+    """Returns commandline args as space-separated string."""
     args = sys.argv[1:]
     txt = " ".join(args) if args else None
     return txt
 
 
 def _getClipboardContent():
+    """Returns the content of the Windows clipboard."""
     return str(pyperclip.paste())
 
 
 def _setClipboardContent(txt):
+    """Sets the Windows clipboard content to the given text."""
     return pyperclip.copy(txt)
 
 
 def _ensurePath(pth):
+    """Tries to get pth either as given, or from commandline or the
+    clipboard. If none of these source provide input, bail out."""
     pth = pth or _getCommandlineInput() or _getClipboardContent()
     if not pth:
         print ("ERROR - Could not get path from commandline "
@@ -51,11 +56,7 @@ def _ensurePath(pth):
 
 
 def _convertWindowsDriveLetter(pth):
-    """Converts 'C:\' to '/C/'.
-
-    Handles wrapping quotation marks.
-
-    """
+    """Converts 'C:\' to '/C/'. Handles wrapping quotation marks."""
     original = pth
     pth = pth.replace(colon + backslash, slash)
     pth = pth.replace(colon + slash, slash)
@@ -70,21 +71,19 @@ def _convertWindowsDriveLetter(pth):
 
 
 def _escapeWindows(pth):
+    """Escapes certain characters in pth with a backslash."""
     for token in (space, openpar, closepar):
         pth = pth.replace(token, backslash + token)
     return pth
 
 
 def _convertWindowsSlashes(pth):
+    """Replaces all backslashes in pth with slashes."""
     return pth.replace(backslash, slash)
 
 
 def _convertUnixDriveLetter(pth):
-    """Converts '/C/' to 'C:\'.
-
-    Handles wrapping quotation marks.
-
-    """
+    """Converts '/C/' to 'C:\'. Handles wrapping quotation marks."""
     starts_with_unix_driveletter = r"([\"\']*)(\/)(?P<drive>[a-zA-Z])(\/)"
     match = re.match(starts_with_unix_driveletter, pth)
     if match:
@@ -99,16 +98,19 @@ def _convertUnixDriveLetter(pth):
 
 
 def _convertUnixSlashes(pth):
+    """Replaces all slashes in pth with backslashes."""
     return pth.replace(slash, backslash)
 
 
 def _unescapeUnix(pth):
+    """Removes backslashes in front of certain characters in pth."""
     for token in (space, openpar, closepar):
         pth = pth.replace(backslash + token, token)
     return pth
 
 
 def _toWindows(pth):
+    """Converts the given Unix path to Windows convention."""
     pth = _convertUnixDriveLetter(pth)
     pth = _convertUnixSlashes(pth)
     pth = _unescapeUnix(pth)
@@ -117,6 +119,7 @@ def _toWindows(pth):
 
 
 def _toUnix(pth):
+    """Converts the given Windows path to Unix convention."""
     pth = _convertWindowsDriveLetter(pth)
     pth = _convertWindowsSlashes(pth)
     pth = _escapeWindows(pth)
@@ -125,10 +128,12 @@ def _toUnix(pth):
 
 
 def sl(pth=None):
+    """Fetches the pth and converts it to Unix convention."""
     pth = _ensurePath(pth)
     return _toUnix(pth)
 
 
 def bsl(pth=None):
+    """Fetches the pth and converts it to Windows convention."""
     pth = _ensurePath(pth)
     return _toWindows(pth)
