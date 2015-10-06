3f7e3af8bb52e12984e6073460ed6bf4f8e02905
Christoph Buelter <c.buelter@arcor.de>
2015-10-07 00:02:49 +0200

    Extend testcases to check for wrapping quotation marks support.

--- tests/test_slbsl.py
+++ tests/test_slbsl.py
@@ -1,7 +1,5 @@
 import pytest
 
-import pyperclip
-
 import slbsl
 
 
@@ -9,13 +7,28 @@ import slbsl
 def testcases():
     """Maps windows paths to their unix equivalents."""
     return {
-        r"C:\Program Files (x86)\Autodesk\Backburner\docs" : r"/C/Program\ Files\ \(x86\)/Autodesk/Backburner/docs",
-        r"C:\Users\username\Google Drive\Sublime Text 2.0.2 Portable\SourceCodePro_FontsOnly-1.017\OTF" : r"/C/Users/username/Google\ Drive/Sublime\ Text\ 2.0.2\ Portable/SourceCodePro_FontsOnly-1.017/OTF"
+        # Basic paths.
+        "C:\\": r"/C/",
+        r"C:\path\to\thing": r"/C/path/to/thing",
+
+        # Whitespace and special characters.
+        r"C:\Program Files (x86)\Autodesk\Backburner\docs": r"/C/Program\ Files\ \(x86\)/Autodesk/Backburner/docs",
+        r"C:\Users\username\Google Drive\Sublime Text 2.0.2 Portable\SourceCodePro_FontsOnly-1.017\OTF": r"/C/Users/username/Google\ Drive/Sublime\ Text\ 2.0.2\ Portable/SourceCodePro_FontsOnly-1.017/OTF",
+
+        # Wrapped in "" quotation marks.
+        r'"C:\"': r'"/C/"',
+        r'"""C:\"""': r'"""/C/"""',
+        r'"C:\Program Files (x86)\Autodesk\Backburner\docs"': r'"/C/Program\ Files\ \(x86\)/Autodesk/Backburner/docs"',
+
+        # Wrapped in '' quotation marks.
+        r"'C:\'": r"'/C/'",
+        r"'''C:\'''": r"'''/C/'''",
+        r"'C:\Program Files (x86)\Autodesk\Backburner\docs'": r"'/C/Program\ Files\ \(x86\)/Autodesk/Backburner/docs'",
     }
 
 
 def test_datavalid(testcases):
-    assert len(testcases) == 2
+    assert len(testcases) == 10
 
 
 def test_sl(testcases):
