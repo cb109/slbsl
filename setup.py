46d88bde591300ba2ae0bea692ec282c384939ef
Christoph Buelter <c.buelter@arcor.de>
2015-10-20 23:15:00 +0200

    Bump version to 0.2.3.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name="slbsl",
-    version="0.2.1",
+    version="0.2.3",
     description="Convert paths from Windows to UNIX and vice versa.",
     long_description=open("README.rst").read(),
     keywords="slash, backslash, linux, unix, windows, conversion",
